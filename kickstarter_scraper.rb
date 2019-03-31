2d4d1e3273bf7aac29549ad5077f740abea99598
Eric Storey <estorey@gmail.com>
2019-03-31 00:48:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects= kickstarter.css("li.project.grid_4")
   binding.pry
+  
+  #location: projects.css("ul.project-meta li a span.location-name").text
 end
 
 create_project_hash
